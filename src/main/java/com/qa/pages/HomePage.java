df90825ca6a9fc9883800072c319b3ea3a8fffa0
arahate <arahate@SWLL124.ap.corp.jwt.com>
2020-01-02 18:39:49 +0530

    test commit changes

--- src/main/java/com/qa/pages/HomePage.java
+++ src/main/java/com/qa/pages/HomePage.java
@@ -13,6 +13,7 @@ public class HomePage extends BootsBaseClass
 
 //page factory
 //page factory changes
+	//test comments
 //Accept cookies button
 @FindBy(xpath="//*[contains(text(), 'Accept Cookies')]")
 WebElement cookieBtn;
